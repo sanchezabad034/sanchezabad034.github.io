5d90977a63e2398b023bf505587622750a1c9bfc
sanchezabad034 <68512055+sanchezabad034@users.noreply.github.com>
2022-03-27 21:04:06 -0600

    Update and remove documents

--- Public/JS/styles.js
+++ Public/JS/styles.js
@@ -1,6 +1,17 @@
-const btnmenu= document.querySelector("btn-menu");
-const menu= document.querySelector("nav-menu");
+//Aqui le estoy diciendo que de clases de mi css voy a utilizar en js
+const navmenu = document.querySelector('.nav-menu');
+const btnmenu = document.querySelector('.btn-menu');
+const closemenu = document.querySelector('.closeMenu');
 
-btnmenu.addEventListener("click", ()=>{
-    menu.classList.toggle("nav-menu_visible");
-});
+//Le estoy agregando los eventos a las clases btn-menu y close menu para mostrar y cerrar el menu
+btnmenu.addEventListener('click', show);
+closemenu.addEventListener('click', close);
+//Aqui es lo que va a realizar cuando el boton de hamburguesa es seleccionado
+function show(){
+    navmenu.style.display='flex'; 
+    navmenu.style.left='0'; 
+}
+//Cuando se cierre el boton de hamburguesa
+function close(){
+    navmenu.style.left='-100%'; 
+}
